c4f8f3d46a85ddd1db70bbcdd77326d3e05c2c88
Stas996 <sai199_@ukr.net>
2017-03-09 22:30:05 +0200

    for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  private
+  protected
     def current_user
       User.where(id: session[:user_id]).first
     end
